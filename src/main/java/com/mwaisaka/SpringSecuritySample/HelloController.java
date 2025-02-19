dc33afed28e1ae8fb1bff8772d1aec3f3a703c87
Phenny <mwaisakaphenny@gmail.com>
2025-02-19 03:54:38 +0300

    Creating custom login for one user

---
+++ src/main/java/com/mwaisaka/SpringSecuritySample/HelloController.java
@@ -0,0 +1,14 @@
+package com.mwaisaka.SpringSecuritySample;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet(HttpServletRequest request){
+        return "Welcome to Mwaisaka's Cult :) \n\nSession ID: " + request.getSession().getId();
+    }
+}
---
+++ src/test/java/com/mwaisaka/SpringSecuritySample/SpringSecuritySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwaisaka.SpringSecuritySample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
