27bc3f50e94ae1300a7afa4cbb6499f7a4e10d39
Phenny <mwaisakaphenny@gmail.com>
2025-02-24 04:53:16 +0300

    Verifying user from database. PS: the databse has not been implemented yet, the rest of the detils have been dealt with

--- src/main/java/com/mwaisaka/SpringSecuritySample/repository/UserRepository.java
+++ src/main/java/com/mwaisaka/SpringSecuritySample/repository/UserRepository.java
@@ -2,6 +2,9 @@ package com.mwaisaka.SpringSecuritySample.repository;
 
 import com.mwaisaka.SpringSecuritySample.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<Users, Integer> {
+    Users findByUsername(String username);
 }
--- src/main/java/com/mwaisaka/SpringSecuritySample/service/MyUserDetailsService.java
+++ src/main/java/com/mwaisaka/SpringSecuritySample/service/MyUserDetailsService.java
@@ -1,5 +1,9 @@
 package com.mwaisaka.SpringSecuritySample.service;
 
+import com.mwaisaka.SpringSecuritySample.model.UserPrincipal;
+import com.mwaisaka.SpringSecuritySample.model.Users;
+import com.mwaisaka.SpringSecuritySample.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,8 +12,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private UserRepository repository
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+
+        Users user = repository.findByUsername(username);
+
+        if (user == null){
+            System.out.println("User not Found!"); //prints in the console
+            throw new UsernameNotFoundException("User not Found!")
+        }
+
+        return new UserPrincipal(user);
     }
 }
